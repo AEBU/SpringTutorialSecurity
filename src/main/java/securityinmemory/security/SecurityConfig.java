0eaccbc0c5fe02894f2ce2afe34854eafc407004
Alexis Bautista <rhack_100@outlook.com>
2017-04-01 17:00:43 -0500

    :SecurityWeb

--- src/main/java/securityinmemory/security/SecurityConfig.java
+++ src/main/java/securityinmemory/security/SecurityConfig.java
@@ -1,7 +1,42 @@
 package securityinmemory.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 /**
  * Created by Alexis on 01/04/2017.
  */
-public class SecurityConfig {
+@Configuration
+public class SecurityConfig  extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/","/home").permitAll()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+        http.exceptionHandling().accessDeniedPage("/403");
+    }
+
+    //Como podemos observar vamos a usar usuarios que esten ya en memoria
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("user").roles("USER")
+                .and()
+                .withUser("admin").password("admin").roles("ADMIN");
+    }
+
 }
